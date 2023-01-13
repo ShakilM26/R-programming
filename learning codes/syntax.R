d363ec79b0632b0421503eed94fd2a0a7cd00b53
Shakil Almirr <101876451+ShakilM26@users.noreply.github.com>
2023-01-13 22:13:32 +0600

    Add files via upload

--- learning codes/syntax.R
+++ learning codes/syntax.R
@@ -1,1 +1,15 @@
+# Syntax 
 
+# Variable
+# In R, there are three ways to write variables
+
+var1 = "Shakil"
+var2 <- "Mou"
+"Shakil & Mou" -> var3
+
+print(var1)
+print(var2)
+print(var3)
+
+# Why does the output begin with [1]? In R, each line of the answer begins with [i] indicating that 
+# the first element of the object displayed on that line is the 1st element of that object. 
